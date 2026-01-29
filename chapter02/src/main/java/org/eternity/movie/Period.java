22e72e6767a4ac590bd91cdc2a821014227b9b88
CHANHAN <130114269+chanani@users.noreply.github.com>
2026-01-29 22:00:29 +0900

    feat : 시간 예외처리 추가

--- chapter02/src/main/java/org/eternity/movie/Period.java
+++ chapter02/src/main/java/org/eternity/movie/Period.java
@@ -8,8 +8,15 @@ public class Period {
     private LocalTime endTime;
 
     public Period(LocalTime startTime, LocalTime endTime) {
+        validateTime(startTime, endTime);
         this.startTime = startTime;
         this.endTime = endTime;
+    }
+
+    private void validateTime(LocalTime startTime, LocalTime endTime) {
+        if(startTime.isAfter(endTime)) {
+            throw new IllegalArgumentException("종료 시간이 시작 시간보다 빠를 수 없습니다.");
+        }
     }
 
     public boolean containsTime(LocalTime time) {
