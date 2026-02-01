be79404062f230f085a783800741b08a2891aca4
모재영 <mo@utransfer.com>
2026-02-01 15:37:22 +0900

    feat : 과제1. 데이터 중심의 영화 예매 시스템 구현 타이핑 하기

--- chapter04/src/main/java/org/eternity/movie/pratice/Movie.java
+++ chapter04/src/main/java/org/eternity/movie/pratice/Movie.java
@@ -1,5 +1,100 @@
 package org.eternity.movie.pratice;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import org.eternity.money.Money;
+
 public class Movie {
 
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private Double discountPercent;
+
+    public Movie(String title, Duration runningTime, Money fee, Money discountAmount, DiscountCondition... discountConditions) {
+        this(title, runningTime, fee, MovieType.AMOUNT_DISCOUNT, discountAmount, null, discountConditions);
+    }
+
+    public Movie(String title, Duration runningTime, Money fee, Double discountPercent, DiscountCondition... discountConditions) {
+        this(title, runningTime, fee, MovieType.PERCENT_DISCOUNT, null, discountPercent, discountConditions);
+    }
+
+    public Movie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        this(title, runningTime, fee, MovieType.NONE_DISCOUNT, null, null, discountConditions);
+    }
+
+    public Movie(String title, Duration runningTime, Money fee, MovieType movieType, Money discountAmount, Double discountPercent, DiscountCondition... discountConditions) {
+        this(title, runningTime, fee, movieType, discountAmount, discountPercent, Arrays.asList(discountConditions));
+    }
+
+    public Movie(String title, Duration runningTime, Money fee, MovieType movieType, Money discountAmount, Double discountPercent, List<DiscountCondition> discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = discountConditions;
+        this.movieType = movieType;
+        this.discountAmount = discountAmount;
+        this.discountPercent = discountPercent;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Duration getRunningTime() {
+        return runningTime;
+    }
+
+    public void setRunningTime(Duration runningTime) {
+        this.runningTime = runningTime;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return discountConditions;
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
 }
--- chapter04/src/main/java/org/eternity/movie/pratice/ReservationAgency.java
+++ chapter04/src/main/java/org/eternity/movie/pratice/ReservationAgency.java
@@ -1,5 +1,46 @@
 package org.eternity.movie.pratice;
 
+import org.eternity.money.Money;
+
 public class ReservationAgency {
 
+    public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
+        Movie movie = screening.getMovie();
+
+        // 할인 가능 여부 검증
+        boolean discountable = false;
+        for (DiscountCondition condition : movie.getDiscountConditions()) {
+            if (condition.getType() == DiscountConditionType.PERIOD) {
+                discountable = screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek()) &&
+                    condition.getStartTime().compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
+                    condition.getEndTime().compareTo(screening.getWhenScreened().toLocalTime()) >= 0;
+            } else {
+                discountable = condition.getSequence() == screening.getSequence();
+            }
+            if (discountable) {
+                break;
+            }
+        }
+
+        // 최종 금액 계산
+        Money fee;
+        if (discountable) {
+            Money discountAmount = Money.ZERO;
+            switch (movie.getMovieType()) {
+                case AMOUNT_DISCOUNT:
+                    discountAmount = movie.getDiscountAmount();
+                    break;
+                case PERCENT_DISCOUNT:
+                    discountAmount = movie.getFee().times(movie.getDiscountPercent());
+                    break;
+                case NONE_DISCOUNT:
+                    discountAmount = Money.ZERO;
+                    break;
+            }
+            fee = movie.getFee().minus(discountAmount);
+        } else {
+            fee = movie.getFee();
+        }
+        return new Reservation(customer, screening, fee, audienceCount);
+    }
 }
