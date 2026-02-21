d8d37530f7072aac9a1f0b71715ede7424919903
CHANHAN <130114269+chanani@users.noreply.github.com>
2026-02-21 20:32:36 +0900

    Feat : Chapter6 theater step1의 데이터 pratice 패키지로 복사

---
+++ chapter06/src/main/java/org/eternity/theater/pratice/Audience.java
@@ -0,0 +1,14 @@
+package org.eternity.theater.pratice;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
+
