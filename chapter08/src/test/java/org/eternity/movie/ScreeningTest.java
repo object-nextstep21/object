9fc508a81231bc5da64bd652b309a6439fb8f03d
CHANHAN <130114269+chanani@users.noreply.github.com>
2026-02-21 22:02:04 +0900

    refactor : 과제2 Setter 메서드 통해 주입 테스트 코드 작성

--- chapter08/src/test/java/org/eternity/movie/ScreeningTest.java
+++ chapter08/src/test/java/org/eternity/movie/ScreeningTest.java
@@ -2,6 +2,8 @@ package org.eternity.movie;
 
 
 import org.eternity.money.Money;
+import org.eternity.movie.pricing.AmountDiscountPolicy;
+import org.eternity.movie.pricing.SequenceCondition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -27,5 +29,25 @@ class ScreeningTest {
 
         assertThat(expectedFee).isEqualTo(Money.wons(7200));
     }
+
+    @Test
+    @DisplayName("setter 메서드로 할인 정책 주입")
+    void setterMethod() {
+        Movie movie = new Movie(
+                "아바타",
+                Duration.ofMinutes(120),
+                Money.wons(8000)
+        );
+
+        movie.changeDiscountPolicy(new AmountDiscountPolicy(Money.wons(1000), new SequenceCondition(1)));
+
+        Screening screening = new Screening(movie, 1, LocalDateTime.of(2026, 02, 21, 21, 39, 0));
+
+        Money expectedFee = movie.calculateMovieFee(screening);
+
+        assertThat(expectedFee).isEqualTo(Money.wons(7000));
+    }
+
+
 
 }
